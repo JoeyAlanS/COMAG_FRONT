01467cb89e3da5a08c69ec9aebf8f663407e20dc
said-gomes <saidgomes09@gmail.com>
2025-05-16 00:21:14 -0300

    ajustes UI/UX, refatoracoes

---
+++ src/components/ui/Carousel.tsx
@@ -0,0 +1,36 @@
+import Carousel from 'react-bootstrap/Carousel';
+import SlideImage from './SlideImage';
+
+export function ImgCarousel() {
+  const imageClassName = "d-block w-100 carousel-image";
+  return (
+    <>
+    <Carousel
+    className={imageClassName}
+    style = {{marginTop:"100px", height:"0px"}}>
+      <Carousel.Item>
+      <SlideImage text="primeiro" />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <SlideImage text="segundo" />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <SlideImage text="terceiro" />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <SlideImage text="quarto" />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </>
+  );
+}
+
+export default ImgCarousel;
